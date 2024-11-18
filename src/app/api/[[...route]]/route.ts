e47e4153b5e0e94119c5a234bacc2f6f2aa00cd4
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-11-18 15:54:16 +0530

    feat: create members page and its function

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,10 +3,14 @@ import { handle } from "hono/vercel";
 
 import authRouter from "@/features/auth/server/route";
 import workspaces from "@/features/workspaces/server/route";
+import members from "@/features/members/server/route";
 
 const app = new Hono().basePath("/api");
 
-const router = app.route("/auth", authRouter).route("/workspaces", workspaces);
+const router = app
+  .route("/auth", authRouter)
+  .route("/workspaces", workspaces)
+  .route("/members", members);
 
 export const GET = handle(app);
 export const POST = handle(app);
