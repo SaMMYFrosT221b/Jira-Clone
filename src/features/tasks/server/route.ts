2dfbf663c2dceeeae29fc0d48cb06f29e54d0311
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-11-20 21:39:41 +0530

    feat: create table and filter option for task

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -6,7 +6,7 @@ import { getMember } from "@/features/members/utils";
 import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID } from "@/config";
 import { ID, Query } from "node-appwrite";
 import { object, z } from "zod";
-import { TaskStatus } from "../types";
+import { Task, TaskStatus } from "../types";
 import { createAdminClient } from "@/lib/appwrite";
 import { Project } from "@/features/projects/types";
 
@@ -48,7 +48,11 @@ const app = new Hono()
       if (dueDate) query.push(Query.equal("dueDate", dueDate));
       if (search) query.push(Query.search("name", search));
 
-      const tasks = await databases.listDocuments(DATABASE_ID, TASKS_ID, query);
+      const tasks = await databases.listDocuments<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        query
+      );
 
       const projectIds = tasks.documents.map((task) => task.projectId);
       const assigneeIds = tasks.documents.map((task) => task.assigneeId);
@@ -91,7 +95,7 @@ const app = new Hono()
         };
       });
 
-      return c.json({ data: { ...tasks, docuemnts: populatedTasks } });
+      return c.json({ data: { ...tasks, documents: populatedTasks } });
     }
   )
   .post(
